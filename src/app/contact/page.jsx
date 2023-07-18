f6d3237808f53b1b38266e7c958d3b962e61d872
Ganesh Shinde <sindheganesh2004@gmail.com>
2023-07-18 22:52:56 +0530

    Feature: Created the contact and home page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import styles from "./page.module.css"
+import React from "react";
+import styles from "./page.module.css";
+import Image from "next/image";
+import Button from "@/components/Button/Button";
 
 function Contact() {
   return (
-    <div className={styles.container}>Contact</div>
-  )
+    <div className={styles.container}>
+      <h1 className={styles.title}>Let's Keep in Touch</h1>
+      <div className={styles.content}>
+        <div className={styles.imgContainer}>
+          <Image
+            src="/contact.png"
+            alt="NexTech"
+            fill={true}
+            className={styles.image}
+          />
+        </div>
+        <form className={styles.form}>
+          <input type="text" placeholder="name" className={styles.input}/>
+          <input type="text" placeholder="email" className={styles.input}/>
+          <textarea className={styles.textArea} placeholder="message" cols="30" rows="10"></textarea>
+          <Button url="#" text="Send" />
+        </form>
+      </div>
+    </div>
+  );
 }
 
-export default Contact
+export default Contact;
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,19 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import Hero from "public/hero.png"
+import Button from '@/components/Button/Button'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>Better design for your digital products</h1>
+        <p className={styles.desc}>Turing your Idea into Reality. We bring together the teams from the global tech industry</p>
+        <Button url="/portfolio" text="See Our Works" />
+      </div>
+      <div className={styles.item}>
+        <Image src={Hero} alt='NexTech' className={styles.img} />
+      </div>
+    </div>
   )
 }
