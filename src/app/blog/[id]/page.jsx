6454ac1635342712b7b1a58e080c0c93c895b98e
Ganesh Shinde <sindheganesh2004@gmail.com>
2023-07-19 22:14:57 +0530

    feature: dynamic changing the page title

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -7,7 +7,7 @@ async function getData(id) {
   const res = await fetch(`http://localhost:3000/api/posts/${id}`, {
     cache: "no-store",
   });
-
+ 
   if (!res.ok) {
     return notFound()
   }
@@ -15,14 +15,12 @@ async function getData(id) {
   return res.json();
 }
 
-
-export async function generateMetadata({ params }) {
-
+export async function generateMetadata({params}) {
   const post = await getData(params.id)
   return {
     title: post.title,
     description: post.desc,
-  };
+  }
 }
 
 const BlogPost = async ({ params }) => {
--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -3,6 +3,11 @@ import styles from "./page.module.css";
 import Image from "next/image";
 import Button from "@/components/Button/Button";
 
+export const metadata = {
+  title: "NexTech Contact",
+  description: "Let's Get Connected ",
+}
+
 function Contact() {
   return (
     <div className={styles.container}>
